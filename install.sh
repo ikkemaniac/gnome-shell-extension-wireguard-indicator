55749703f98e43fcecf9715a2a351c24753efb5a
ikkemaniac <ikkemaniac@localhost>
2020-08-30 15:06:52 +0200

    have install.sh call 'tools.sh install'

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ dir=~/.local/share/gnome-shell/extensions/wireguard-indicator@gregos.me
 rm -rf $dir
 mkdir -p $dir
 cp -R . $dir
+
+#'install' extension - needs to put all the required SUDO cmd-s in a SUDOERS file:
+$(${dir}/src/tools.sh install)
